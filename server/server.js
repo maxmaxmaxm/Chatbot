9d88ac4b7b098ae8c7b334af4fb55f6c42f2a423
Max <maximilian.weinhold@gmail.com>
2023-05-24 11:11:41 +0200

    rollback

--- server/server.js
+++ server/server.js
@@ -10,8 +10,6 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const apiUrl = 'https://api.openai.com/v1/completions';
-
 
 const app = express()
 app.use(cors())
@@ -27,10 +25,10 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0.3, // Higher values means the model will take more risks.
+      temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
